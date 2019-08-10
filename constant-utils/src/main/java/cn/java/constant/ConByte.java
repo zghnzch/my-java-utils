4c2726bf6e6828bd57507eff09bc8d737deb5dc4
zghnzch <zghnzch@hotmail.com>
2019-08-10 13:38:55 +0800

    Initial commit

---
+++ constant-utils/src/main/java/cn/java/constant/ConByte.java
@@ -0,0 +1,5 @@
+package cn.java.constant;
+/**
+ * @author zch
+ */
+public class ConByte {}
---
+++ constant-utils/src/main/java/cn/java/constant/ConStr.java
@@ -0,0 +1,118 @@
+package cn.java.constant;
+/**
+ * @author zchcpy
+ * 190505
+ */
+public class ConStr {
+	/**
+	 * 当前登录帐号
+	 */
+	public final static String LOGINNAME_ADMIN = "admin";
+	/**
+	 * userNOUnique
+	 */
+	public final static String USERNOUNIQUE = "userNOUnique";
+	/**
+	 * null
+	 */
+	public final static String NULLSTR = "null";
+	/**
+	 * user status
+	 */
+	public final static String STR_STATUS = "status";
+	/*--str 形式的数字------------------*/
+	/**
+	 * 未发卡用户状态为0
+	 */
+	public final static String STRZ0 = "0";
+	public final static String STRP1 = "1";
+	public final static String STRP2 = "2";
+	public final static String STRP3 = "3";
+	public final static String STRP4 = "4";
+	public final static String STRP5 = "5";
+	public final static String STRP6 = "6";
+	public final static String STRP7 = "7";
+	public final static String STRP8 = "8";
+	public final static String STRP9 = "9";
+	public final static String STRP10 = "10";
+	/**
+	 * 正常卡用户状态为0
+	 */
+	public final static String STRP241 = "241";
+	/**
+	 * 挂失卡用户状态为0
+	 */
+	public final static String STRP243 = "243";
+	/**
+	 * 注销卡用户状态为0
+	 */
+	public final static String STRP244 = "244";
+	public final static String STRN1 = "-1";
+	/**
+	 * os
+	 */
+	public final static String WINDOWS = "windows";
+	public final static String LINUX = "linux";
+	public final static String RETURN_MSG = "return_msg";
+	public final static String OK = "OK";
+	public final static String RESULT_CODE = "result_code";
+	public final static String SUCCESS = "SUCCESS";
+	/**
+	 * 路径
+	 */
+	public final static String PATH_PAYBOOK = "/Wx/paybooks/";
+	/**
+	 * 无卡支付 用水金额命令
+	 */
+	public final static String CMD_NOCARDMONEY = "nocardPay_imdMony";
+	/**
+	 * 无卡支付用户停水命令
+	 */
+	public final static String CMD_NOCARDSTOP = "nocardPay_imdStop";
+	/**
+	 * 用户充电命令
+	 */
+	public final static String CMD_BEGINCHARGE = "power_begin_charge";
+	/**
+	 * 用户停电命令
+	 */
+	public final static String CMD_STOPCHARGE = "power_stop_charge";
+	/**
+	 * 用户电吹风开始命令
+	 */
+	public final static String CMD_DRIERBEGINCHARGE = "drier_begin_charge";
+	/**
+	 * 用户电吹风停止命令
+	 */
+	public final static String CMD_DRIERSTOPCHARGE = "drier_stop_charge";
+	/**
+	 * 常用汉字
+	 */
+	public final static String WXUSERDEPTNAME = "微信关注用户";
+	/**
+	 * 无连接符精确到秒
+	 */
+	public final static String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+	public final static String YYYY_MM_DD = "yyyy-MM-dd";
+	public final static String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+	public final static String HH_MM_SS = "HH:mm:ss";
+	/**
+	 * 常用固定时间
+	 */
+	public final static String CARDENDTIME = "2099-12-30";
+	public final static String DEFAULT_IDENTITY_CARD = "123456789012345678";
+	public final static String DEFAULT_PHONE = "12345678910";
+	public final static String DEFAULT_CONSUME_PWD = "6666";
+	/**
+	 * 微信支付宝相关String
+	 */
+	public final static String OPENID = "openid";
+	public final static String TIME_END = "time_end";
+	public final static String TEXT = "text";
+	public final static String VOICE = "voice";
+	public final static String EVENT = "event";
+	/**
+	 * 常用标点符号
+	 */
+	public final static String COMMA = ",";
+}
